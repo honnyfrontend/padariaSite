46019b01e704164c8b9a980919c419ee7976c196
honny batista de oliveira <honnyfrontend@gmail.com>
2023-09-07 12:46:15 -0300

    implementando   slide no banner

---
+++ index.js
@@ -0,0 +1,16 @@
+var primeiro =  document.getElementById('div-um')
+var segundo =  document.getElementById('div-dois')
+
+function slide(n){
+	const loop = setInterval(()=>{
+		if(n % 2 === 0 ){
+			primeiro.setAttribute('class','um')
+			segundo.setAttribute('class','dois')
+		}else{
+			primeiro.setAttribute('class','dois')
+			segundo.setAttribute('class','um')
+		}
+		n --
+	},1000)
+}
+slide(100)
